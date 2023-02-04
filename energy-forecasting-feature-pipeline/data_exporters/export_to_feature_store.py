88f0a659773db62a42ab26a7da40b5e7a382947b
kurtispykes <kurtispykes@gmail.com>
2023-02-04 14:12:51 +0200

    Add data exporter templaate

---
+++ energy-forecasting-feature-pipeline/data_exporters/export_to_feature_store.py
@@ -0,0 +1,20 @@
+from pandas import DataFrame
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+
+@data_exporter
+def export_data(df: DataFrame, **kwargs):
+    """
+    Exports data to some source
+
+    Args:
+        df (DataFrame): Data frame to export to
+
+    Output (optional):
+        Optionally return any object and it'll be logged and
+        displayed when inspecting the block run.
+    """
+    # Specify your data exporting logic here
+
