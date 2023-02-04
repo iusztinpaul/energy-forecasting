b997379fc904336d83d56ee0fbf8a7fdd9520b23
Paul Iusztin <paul.lusztin@coreai.ai>
2023-02-04 13:37:43 +0200

    feat: Add mage ai template.

---
+++ energy-forecasting-feature-pipeline/data_loaders/ingest_hourly_energy_consumption.py
@@ -0,0 +1,26 @@
+import io
+import pandas as pd
+import requests
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_api(*args, **kwargs):
+    """
+    Template for loading data from API
+    """
+    url = ''
+
+    response = requests.get(url)
+    return pd.read_csv(io.StringIO(response.text), sep=',')
+
+
+@test
+def test_output(df, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert df is not None, 'The output is undefined'
