b1d7dc423e44a7e9d8b2c97127e50084488aad28
IusztinPaul <p.e.iusztin@gmail.com>
2023-02-17 14:44:36 +0200

    refactor: Refactor preprocessing methods to load data only once

--- training-pipeline/preprocess.py
+++ training-pipeline/preprocess.py
@@ -4,9 +4,11 @@ import pandas as pd
 from category_encoders import hashing, one_hot
 
 
-def split_data(df: pd.DataFrame, **kwargs) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+def split_data(
+    df: pd.DataFrame, **kwargs
+) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     """
-    Template for splitting data into train and test sets.
+    Template for splitting data into train, validation and test sets.
 
     Args:
         df: Dataframe containing the data.
@@ -19,13 +21,13 @@ def split_data(df: pd.DataFrame, **kwargs) -> Tuple[pd.DataFrame, pd.DataFrame, 
 
     max_datetime = df["datetime_utc"].max()
     min_test_split_datetime = max_datetime - pd.DateOffset(days=test_split_size_days)
-    min_validation_split_datetime = max_datetime - pd.DateOffset(days=test_split_size_days + validation_split_size_days)
+    min_validation_split_datetime = max_datetime - pd.DateOffset(
+        days=test_split_size_days + validation_split_size_days
+    )
 
     train_mask = df["datetime_utc"] < min_validation_split_datetime
     validation_mask = df["datetime_utc"].between(
-        min_validation_split_datetime,
-        min_test_split_datetime,
-        inclusive="left"
+        min_validation_split_datetime, min_test_split_datetime, inclusive="left"
     )
     test_mask = df["datetime_utc"] >= min_test_split_datetime
 
@@ -37,9 +39,9 @@ def split_data(df: pd.DataFrame, **kwargs) -> Tuple[pd.DataFrame, pd.DataFrame, 
 
 
 def encode_categorical(
-        train_df: pd.DataFrame,
-        validation_df: pd.DataFrame,
-        test_df: pd.DataFrame,
+    train_df: pd.DataFrame,
+    validation_df: pd.DataFrame,
+    test_df: pd.DataFrame,
 ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     """
     Encode categorical variables
@@ -53,12 +55,16 @@ def encode_categorical(
         DataFrame: Transformed data frames
     """
 
-    consumer_type_hashing_encoder = hashing.HashingEncoder(return_df=True, cols=["consumer_type"])
+    consumer_type_hashing_encoder = hashing.HashingEncoder(
+        return_df=True, cols=["consumer_type"]
+    )
     train_df = consumer_type_hashing_encoder.fit_transform(train_df)
     validation_df = consumer_type_hashing_encoder.transform(validation_df)
     test_df = consumer_type_hashing_encoder.transform(test_df)
 
-    area_one_hot_encoder = one_hot.OneHotEncoder(return_df=True, cols=["area"], handle_unknown="error")
+    area_one_hot_encoder = one_hot.OneHotEncoder(
+        return_df=True, cols=["area"], handle_unknown="error"
+    )
     train_df = area_one_hot_encoder.fit_transform(train_df)
     validation_df = area_one_hot_encoder.fit_transform(validation_df)
     test_df = area_one_hot_encoder.transform(test_df)
--- training-pipeline/train.py
+++ training-pipeline/train.py
@@ -1,11 +1,14 @@
 import logging
+from functools import partial
+from typing import Tuple
 
-import joblib
 import lightgbm as lgb
 import pandas as pd
 import wandb
+from sklearn.metrics import mean_squared_error
 
 import preprocess
+import utils
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -17,9 +20,9 @@ WANDB_PROJECT = "energy_consumption"
 #     "method": "grid",
 #     "metric": {"name": "validation.rmse", "goal": "minimize"},
 #     "parameters": {
-#         "n_estimators": {"values": [100, 500, 1000]},
-#         "max_depth": {"values": [3, 5, 7]},
-#         "learning_rate": {"values": [0.05, 0.1, 0.2]},
+#         "n_estimators": {"values": [500, 1000]},
+#         "max_depth": {"values": [3, 5]},
+#         "learning_rate": {"values": [0.05, 0.1]},
 #         "bagging_fraction": {"values": [0.8, 1.0]},
 #         "feature_fraction": {"values": [0.8, 1.0]},
 #         "lambda_l2": {"values": [0.0, 0.01]},
@@ -30,7 +33,7 @@ sweep_configs = {
     "metric": {"name": "validation.rmse", "goal": "minimize"},
     "parameters": {
         "n_estimators": {"values": [100]},
-        "max_depth": {"values": [1, 2, 3]},
+        "max_depth": {"values": [3]},
         "learning_rate": {"values": [0.05]},
         "bagging_fraction": {"values": [0.8]},
         "feature_fraction": {"values": [0.8]},
@@ -40,20 +43,9 @@ sweep_configs = {
 sweep_id = wandb.sweep(sweep=sweep_configs, project="energy_consumption")
 
 
-def find_best_hyperparameters(
-    data_path: str = "../energy_consumption_data.parquet",
-    model_path: str = "../energy_consumption_model.pkl",
-    target: str = "energy_consumption_future_hours_0",
-) -> None:
-    """
-    Template for training a model.
-
-    Args:
-        data_path: Path to the training data.
-        model_path: Path to save the trained model.
-    """
+def get_dataset(data_path: str) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     # load data
-    df = load_data_from_parquet(data_path)
+    df = utils.load_data_from_parquet(data_path)
     # preprocess data
     train_df, validation_df, test_df = preprocess.split_data(df)
     train_df, validation_df, test_df = preprocess.encode_categorical(
@@ -72,9 +64,7 @@ def find_best_hyperparameters(
         run.log_artifact(raw_data_at)
 
     with init_wandb_run(name="train_validation_test_split", job_type="split") as run:
-        data_at = run.use_artifact(
-            "energy_consumption_data:latest"
-        )
+        data_at = run.use_artifact("energy_consumption_data:latest")
         data_dir = data_at.download()
 
         artifacts = {}
@@ -88,44 +78,26 @@ def find_best_hyperparameters(
             }
 
             artifacts[split] = wandb.Artifact(
-                f"{split}_split",
-                type="split",
-                metadata=metadata
+                f"{split}_split", type="split", metadata=metadata
             )
 
         for split, artifact in artifacts.items():
             run.log_artifact(artifact)
 
-    train_experiment(
-        train_df=train_df,
-        validation_df=validation_df,
-        target=target,
-    )
+    return train_df, validation_df, test_df
 
 
-def load_data_from_parquet(data_path: str) -> pd.DataFrame:
-    """
-    Template for loading data from a parquet file.
-
-    Args:
-        data_path: Path to the parquet file.
-
-    Returns: Dataframe with the data.
-    """
-
-    return pd.read_parquet(data_path)
-
-
-def train_experiment(
+def train_sweep(
     train_df: pd.DataFrame,
     validation_df: pd.DataFrame,
-    target: str,
+    target: str = "energy_consumption_future_hours_0",
 ) -> lgb.LGBMRegressor:
     """
     Function that is training a LGBM model.
 
      Args:
-         df: Dataframe containing the training data.
+         train_df: Training data.
+         validation_df: Validation data.
          target: Name of the target column.
 
      Returns: Trained LightGBM model
@@ -140,21 +112,17 @@ def train_experiment(
             f"{WANDB_ENTITY}/{WANDB_PROJECT}/train_split:latest", type="split"
         )
 
-        model = train(df=train_df, target=target, config=config)
+        model = train_lgbm_regressor(df=train_df, target=target, config=config)
 
         # evaluate model
-        rmse = evaluate_model(
-            model, train_df, target=target
-        )
+        rmse = evaluate_model(model, train_df, target=target)
         logger.info(f"Train RMSE: {rmse:.2f}")
         logger.info(
             f"Train Mean Energy Consumption: {train_df['energy_consumption_future_hours_0'].mean():.2f}"
         )
         wandb.log({"train": {"rmse": rmse}})
 
-        rmse = evaluate_model(
-            model, validation_df, target=target
-        )
+        rmse = evaluate_model(model, validation_df, target=target)
         logger.info(f"Train RMSE: {rmse:.2f}")
         logger.info(
             f"Train Mean Energy Consumption: {validation_df['energy_consumption_future_hours_0'].mean():.2f}"
@@ -164,7 +132,7 @@ def train_experiment(
     return model
 
 
-def train(df: pd.DataFrame, target: str, config: dict):
+def train_lgbm_regressor(df: pd.DataFrame, target: str, config: dict):
     model = lgb.LGBMRegressor(
         n_estimators=config["n_estimators"],
         max_depth=config["max_depth"],
@@ -182,8 +150,6 @@ def train(df: pd.DataFrame, target: str, config: dict):
     return model
 
 
-
-# write a function that evaluates the model with rmse
 def evaluate_model(model, df: pd.DataFrame, target: str):
     """
     Template for evaluating a model.
@@ -195,26 +161,12 @@ def evaluate_model(model, df: pd.DataFrame, target: str):
 
     Returns: RMSE
     """
-    from sklearn.metrics import mean_squared_error
 
     feature_columns = list(set(df.columns) - set([target, "datetime_utc"]))
     y_pred = model.predict(df[feature_columns])
     y_true = df[target]
 
     return mean_squared_error(y_true, y_pred, squared=False)
-
-
-# write function that saves the model using joblib
-def save_model(model, model_path: str):
-    """
-    Template for saving a model.
-
-    Args:
-        model: Trained model.
-        model_path: Path to save the model.
-    """
-
-    joblib.dump(model, model_path)
 
 
 def init_wandb_run(
@@ -228,7 +180,9 @@ def init_wandb_run(
 
 
 def train_best_model(
-    data_path: str = "../energy_consumption_data.parquet",
+    train_df: pd.DataFrame,
+    validation_df: pd.DataFrame,
+    test_df: pd.DataFrame,
     model_path: str = "../energy_consumption_model.pkl",
     target: str = "energy_consumption_future_hours_0",
 ) -> None:
@@ -236,8 +190,11 @@ def train_best_model(
     Template for training a model.
 
     Args:
-        data_path: Path to the training data.
+        train_df: Training data.
+        validation_df: Validation data.
+        test_df: Test data.
         model_path: Path to save the trained model.
+        target: Name of the target column.
     """
 
     api = wandb.Api()
@@ -245,24 +202,12 @@ def train_best_model(
     best_run = sweep.best_run()
     config = best_run.config
 
-    # load data
-    df = load_data_from_parquet(data_path)
-    # preprocess data
-    train_df, validation_df, test_df = preprocess.split_data(df)
-    train_df, validation_df, test_df = preprocess.encode_categorical(
-        train_df, validation_df, test_df
-    )
-
     # train on train + validation split to compute test score
-    model = train(
-        df=pd.concat([train_df, validation_df], axis=0),
-        target=target,
-        config=config
+    model = train_lgbm_regressor(
+        df=pd.concat([train_df, validation_df], axis=0), target=target, config=config
     )
 
-    rmse = evaluate_model(
-        model, test_df, target="energy_consumption_future_hours_0"
-    )
+    rmse = evaluate_model(model, test_df, target="energy_consumption_future_hours_0")
     logger.info(f"Test RMSE: {rmse:.2f}")
     logger.info(
         f"Test Mean Energy Consumption: {test_df['energy_consumption_future_hours_0'].mean():.2f}"
@@ -270,12 +215,12 @@ def train_best_model(
 
     # train on everything for the final model
     with init_wandb_run(name="best_model", job_type="best_model") as run:
-        model = train(
+        model = train_lgbm_regressor(
             df=pd.concat([train_df, validation_df, test_df], axis=0),
             target=target,
-            config=config
+            config=config,
         )
-        save_model(model, model_path)
+        utils.save_model(model, model_path)
 
         metadata = dict(config)
         metadata["target"] = target
@@ -285,10 +230,7 @@ def train_best_model(
         The model is trained on {target} as target.
         """
         model_artifact = wandb.Artifact(
-            "LGBMRegressor",
-            type="model",
-            description=description,
-            metadata=metadata
+            "LGBMRegressor", type="model", description=description, metadata=metadata
         )
         # wandb.save(model_path)
         model_artifact.add_file(model_path)
@@ -297,7 +239,17 @@ def train_best_model(
 
 
 if __name__ == "__main__":
-    wandb.agent(project="energy_consumption", sweep_id=sweep_id, function=find_best_hyperparameters)
-    train_best_model()
+    train_df, validation_df, test_df = get_dataset(
+        data_path="../energy_consumption_data.parquet"
+    )
 
-
+    wandb.agent(
+        project="energy_consumption",
+        sweep_id=sweep_id,
+        function=partial(train_sweep, train_df=train_df, validation_df=validation_df),
+    )
+    train_best_model(
+        train_df=train_df,
+        validation_df=validation_df,
+        test_df=test_df,
+    )
